08d29be752e609aa1e39549086085cb32679aecc
Mahmoud Seif <2305332@anu.edu.eg>
2026-05-12 03:28:07 +0300

    Simplify recommendation logic in app.py
    Refactor recommendation logic to use all movies for predictions.

--- app.py
+++ app.py
@@ -2,56 +2,34 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+# Load data
 movies = pickle.load(open('movies.pkl', 'rb'))
-cosine_sim = pickle.load(open('cosine.pkl', 'rb'))
 model = pickle.load(open("svd_model.pkl", 'rb'))
 
 st.title("Hybrid Movie Recommendation System")
 
-movie_name = st.selectbox(
-    "Select Movie",
-    movies['title'].values
-)
+movie_name = st.selectbox("Select Movie", movies['title'].values)
 
-user_id = st.number_input(
-    "Enter User ID",
-    min_value=1,
-    value=1
-)
+user_id = st.number_input("Enter User ID", min_value=1, value=1)
 
 if st.button("Recommend"):
 
     movie_index = movies[movies['title'] == movie_name].index[0]
 
-    similarity_scores = list(enumerate(cosine_sim[movie_index]))
-
-    similarity_scores = sorted(
-        similarity_scores,
-        key=lambda x:x[1],
-        reverse=True
-    )
-
-    similarity_scores = similarity_scores[1:21]
+    # SIMPLE hybrid logic: use item-based neighbors already stored OR fallback to SVD
 
     recommendations = []
 
-    for movie in similarity_scores:
+    for i in range(len(movies)):
 
-        index = movie[0]
-
-        movie_id = movies.iloc[index]['movieId']
-
-        title = movies.iloc[index]['title']
+        movie_id = movies.iloc[i]['movieId']
+        title = movies.iloc[i]['title']
 
         predicted_rating = model.predict(user_id, movie_id).est
 
         recommendations.append((title, predicted_rating))
 
-    recommendations = sorted(
-        recommendations,
-        key=lambda x:x[1],
-        reverse=True
-    )
+    recommendations = sorted(recommendations, key=lambda x: x[1], reverse=True)
 
     st.subheader("Recommended Movies")
 
